70295180c8fda6cd5ada7595ee3d84100e5a0abc
Neil <akashneil@live.com>
2021-10-10 17:50:35 +0400

    .

--- src/main/java/io/github/seebaware/springbootrestapimongodb/employee/EmployeeController.java
+++ src/main/java/io/github/seebaware/springbootrestapimongodb/employee/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/students")
+@RequestMapping("api/v1/employees")
 @AllArgsConstructor
 public class EmployeeController {
 

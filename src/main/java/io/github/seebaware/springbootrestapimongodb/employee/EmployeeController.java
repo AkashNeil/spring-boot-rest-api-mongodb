b459ff8b3a1a63ef3b276b37e83444eee1b88aba
Neil <akashneil@live.com>
2021-10-10 18:07:05 +0400

    Update EmployeeController.java

--- src/main/java/io/github/seebaware/springbootrestapimongodb/employee/EmployeeController.java
+++ src/main/java/io/github/seebaware/springbootrestapimongodb/employee/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/employees")
+@RequestMapping("api/employees")
 @AllArgsConstructor
 public class EmployeeController {
 
